cdf9910a22bdc4c9d17e72ce6df76acf929434e9
KevinSong0428 <ks5138@nyu.edu>
2023-11-21 14:37:51 -0500

    renamed to backend

--- backend/apps.py
+++ backend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdzunaConfig(AppConfig):
+class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adzuna'
+    name = 'backend'
--- job_seeker/settings.py
+++ job_seeker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "adzuna.apps.AdzunaConfig",
+    "backend.apps.BackendConfig",
     "rest_framework"
 ]
 
